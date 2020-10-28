532ae6d5a77122af7825804184d09466908f4c36
Front-endNoob <stivem333666@gmail.com>
2020-10-28 20:01:06 +0300

    feat: add shuffle function for Portfolio

--- singolo/js/dist/script.dev.js
+++ singolo/js/dist/script.dev.js
@@ -1,26 +1,49 @@
 "use strict";
 
-// This function for displaying images
-var allElements = function allElements() {
-  var imgArray = ['./src/img/portfolio-1.png', './src/img/portfolio-2.png', './src/img/portfolio-3.png', './src/img/portfolio-4.png', './src/img/portfolio-5.png', './src/img/portfolio-6.png', './src/img/portfolio-7.png', './src/img/portfolio-8.png', './src/img/portfolio-9.png', './src/img/portfolio-10.png', './src/img/portfolio-11.png', './src/img/portfolio-12.png'];
-  var portfolio = document.querySelector('.portfolio-images');
-  imgArray.forEach(function (el) {
-    var divItems = document.createElement('div');
-    var image = document.createElement('img');
-    divItems.setAttribute('class', 'portfolio-images__item');
-    image.setAttribute('src', el);
-    divItems.appendChild(image);
-    portfolio.appendChild(divItems);
+// This Array with images for portfolio
+var imgArray = ['./src/img/portfolio-1.png', './src/img/portfolio-2.png', './src/img/portfolio-3.png', './src/img/portfolio-4.png', './src/img/portfolio-5.png', './src/img/portfolio-6.png', './src/img/portfolio-7.png', './src/img/portfolio-8.png', './src/img/portfolio-9.png', './src/img/portfolio-10.png', './src/img/portfolio-11.png', './src/img/portfolio-12.png']; // This header nav menu
+
+var menu = document.querySelector('.menu'); // This nav menu elements li
+
+var menuItems = menu.querySelectorAll('li'); // This tags menu
+
+var tags = document.querySelector('.tags'); // This tags menu elements li
+
+var tagsItems = tags.querySelectorAll('li'); // This portfolio container for images
+
+var portfolio = document.querySelector('.portfolio-images'); // This images tags in portfolio container
+
+var imgPort = portfolio.querySelectorAll('img'); // Shuffle function
+
+var shuffleImages = function shuffleImages(images) {
+  images.sort(function () {
+    return Math.random() - 0.5;
   });
-};
+  return images;
+}; // Active links for nav menu
 
-allElements(); // Active links for menu
 
-var menu = document.querySelector('.menu');
 menu.addEventListener('click', function (e) {
-  var menuItems = menu.querySelectorAll('li');
   menuItems.forEach(function (el) {
     el.querySelector('a').classList.remove('active_link');
-    e.target.classList.add('active_link');
   });
-});
+  e.target.classList.add('active_link');
+}); // Active tags for tags menu and shuffle Images in Portfolio
+
+tags.addEventListener('click', function (e) {
+  if (!e.target.classList.contains('active_tag')) {
+    tagsItems.forEach(function (el) {
+      el.classList.remove('active_tag');
+    });
+    e.target.classList.add('active_tag');
+    allImgPortfolio(shuffleImages(imgArray), imgPort);
+  }
+}); // This function for displaying images into portfolio
+
+var allImgPortfolio = function allImgPortfolio(array, imgPort) {
+  for (var i = 0; i < array.length; i++) {
+    imgPort[i].setAttribute('src', array[i]);
+  }
+};
+
+allImgPortfolio(imgArray, imgPort);
--- singolo/js/script.js
+++ singolo/js/script.js
@@ -1,46 +1,81 @@
-// This function for displaying images
+// This Array with images for portfolio
+let imgArray = [
+    './src/img/portfolio-1.png',
+    './src/img/portfolio-2.png',
+    './src/img/portfolio-3.png',
+    './src/img/portfolio-4.png',
+    './src/img/portfolio-5.png',
+    './src/img/portfolio-6.png',
+    './src/img/portfolio-7.png',
+    './src/img/portfolio-8.png',
+    './src/img/portfolio-9.png',
+    './src/img/portfolio-10.png',
+    './src/img/portfolio-11.png',
+    './src/img/portfolio-12.png'
+]
 
-const allElements = () => {
-    let imgArray = [
-        './src/img/portfolio-1.png',
-        './src/img/portfolio-2.png',
-        './src/img/portfolio-3.png',
-        './src/img/portfolio-4.png',
-        './src/img/portfolio-5.png',
-        './src/img/portfolio-6.png',
-        './src/img/portfolio-7.png',
-        './src/img/portfolio-8.png',
-        './src/img/portfolio-9.png',
-        './src/img/portfolio-10.png',
-        './src/img/portfolio-11.png',
-        './src/img/portfolio-12.png'
-    ]
+// This header nav menu
+const menu = document.querySelector('.menu');
+// This nav menu elements li
+const menuItems = menu.querySelectorAll('li');
 
-    const portfolio = document.querySelector('.portfolio-images');
+// This tags menu
+const tags = document.querySelector('.tags');
+// This tags menu elements li
+const tagsItems = tags.querySelectorAll('li');
 
-    imgArray.forEach((el) => {
-        const divItems = document.createElement('div');
-        const image = document.createElement('img');
-        divItems.setAttribute('class', 'portfolio-images__item');
-        image.setAttribute('src', el);
-        divItems.appendChild(image);
-        portfolio.appendChild(divItems);
+// This portfolio container for images
+const portfolio = document.querySelector('.portfolio-images');
+// This images tags in portfolio container
+const imgPort = portfolio.querySelectorAll('img')
 
-    })
+// Shuffle function
+const shuffleImages = (images) => {
+    images.sort(() => Math.random() - 0.5);
+    return images;
 }
 
-allElements();
-
-
-// Active links for menu
-
-const menu = document.querySelector('.menu');
+// Active links for nav menu
 menu.addEventListener('click', e => {
-    const menuItems = menu.querySelectorAll('li');
     menuItems.forEach(el => {
         el.querySelector('a').classList.remove('active_link');
-        e.target.classList.add('active_link')
     })
+    e.target.classList.add('active_link')
 })
+
+// Active tags for tags menu and shuffle Images in Portfolio
+tags.addEventListener('click', e => {
+    if(!e.target.classList.contains('active_tag')) {
+        tagsItems.forEach(el => {
+            el.classList.remove('active_tag');
+    })
+        e.target.classList.add('active_tag');
+        allImgPortfolio(shuffleImages(imgArray), imgPort);
+    }   
+})
+
+// This function for displaying images into portfolio
+const allImgPortfolio = (array,imgPort) => {
+    
+    for(let i = 0; i < array.length; i++) { 
+         imgPort[i].setAttribute('src', array[i]);
+    }
+}
+
+allImgPortfolio(imgArray,imgPort);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
