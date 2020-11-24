08e7bc0044df09b50be959f750b0487a33786b81
Front-endNoob <stivem333666@gmail.com>
2020-11-24 20:53:37 +0300

    refactor: slightly optimized code and removed unnecessary

--- calulator/js/script.js
+++ calulator/js/script.js
@@ -1,10 +1,6 @@
 // This our elements
-const numbers = document.querySelectorAll('.number');
-const mathOperators = document.querySelectorAll('.operator');
-const clearOperators = document.querySelectorAll(".clear-btn");
-const result = document.getElementById("result");
-const decimal = document.getElementById("decimal");
 const display = document.getElementById("display");
+const buttons = document.querySelector('.calc__buttons');
 
 
 // This state variables 
@@ -20,72 +16,75 @@ let currNumber = 0;
 let expectationOper = '';
 
 
-// Add event for numbers
-numbers.forEach(el => {
-	el.addEventListener('click', e => {
-		if(newNumber) {
-			display.value = e.target.textContent;
-			newNumber = false;
-		}else if(display.value === '0') {
-			display.value = e.target.textContent;
-		}else {
-			display.value += e.target.textContent;
-		}
-	})
-});
+// Adding an event when clicking on an element and processing operations
+buttons.addEventListener('click', event => {
 
-// Add event for Math operators
-mathOperators.forEach(el => {
-	el.addEventListener('click', e => {
-		let memory = display.value;
+	// Digit processing
+    if (event.target.classList.contains('number')) {
 
-		if(newNumber && expectationOper !== '=') {
-			display.value = currNumber;
-		}else {
-			newNumber = true;
-			if(expectationOper === '+') {
-				currNumber += memory;
-			}else if(expectationOper === '-') {
-				currNumber -= memory;
-			}else if(expectationOper === '*') {
-				currNumber *= memory;
-			}else if(expectationOper === '/') {
-				currNumber /= memory;
-			}else {
-				currNumber = +memory;
-			}
-			display.value = currNumber;
-			expectationOper = e.target.textContent;
-		}
-	})
-});
+        if (newNumber) {
+            display.value = event.target.textContent;
+            newNumber = false;
+        } else if (display.value === '0') {
+            display.value = event.target.textContent;
+        } else {
+            display.value += event.target.textContent;
+        }
 
-// Add event for clear operators
-clearOperators.forEach(el => {
-	el.addEventListener('click', e => {
-		if(e.target.textContent === 'ce') {
-			display.value = '0';
-			newNumber = true;
-		}else if(e.target.textContent === 'c') {
-			display.value = '0';
-			newNumber = true;
-			currNumber = 0;
-			expectationOper = '';
-		}
-	})
-});
+    }
 
-// Add event for decimal
-decimal.addEventListener('click', e => {
-	let decMemory = display.value;
+    // Processing 'ce' and 'с'
+    if (event.target.classList.contains('clear-btn')) {
+        display.value = '0';
+        newNumber = true;
+        if (event.target.textContent === 'c') {
+            currNumber = 0;
+            expectationOper = '';
+        }
+    }
 
-	if(newNumber) {
-		decMemory = '0.';
-		newNumber = false;
-	}else {
-		if(decMemory.indexOf('.') === -1) {
-			decMemory += '.'
-		}
-	}
-	display.value = decMemory;
+    // Processing '.'
+    if (event.target.classList.contains('decimal')) {
+        let decMemory = display.value;
+
+        if (newNumber) {
+            decMemory = '0.';
+            newNumber = false;
+        } else {
+            if (decMemory.indexOf('.') === -1) {
+                decMemory += '.'
+            }
+        }
+        display.value = decMemory;
+    }
+
+    // Processing operators
+    if (event.target.classList.contains('operator')) {
+        let memory = display.value;
+
+        if (!newNumber || expectationOper == '=') {
+            newNumber = true;
+
+            switch (expectationOper) {
+                case '+':
+                    currNumber += +memory;
+                    break;
+                case '-':
+                    currNumber -= +memory;
+                    break;
+                case '*':
+                    currNumber *= +memory;
+                    break;
+                case '/':
+                    currNumber /= +memory;
+                    break;
+
+                default:
+                    currNumber = +memory;
+                    break;
+            }
+            expectationOper = event.target.textContent;
+        }
+        display.value = currNumber;
+    }
 })
