17ab9f7d120e3fcaf031c2258998d5e7a702d2d9
Front-endNoob <stivem333666@gmail.com>
2020-11-27 03:16:22 +0300

    feat: add animate for bg

--- MeditationApp/js/script.js
+++ MeditationApp/js/script.js
@@ -1,9 +1,11 @@
 // Elements and Variables
 const appButtons = document.querySelector('.app__buttons');
 const muteBtn = document.querySelector('.mute');
-const pauseBtn = document.querySelector('img')
-const dataBtns = document.querySelectorAll('.data')
+const pauseBtn = document.querySelector('img');
+const pauseBorder = document.querySelector('.btn.pause')
+const dataBtns = document.querySelectorAll('.data');
 const video = document.querySelector('.video');
+const app = document.querySelector('.app');
 let audio = new Audio("./src/audio/nature.mp3");
 
 let data = [{
@@ -45,31 +47,46 @@ appButtons.addEventListener('click', event => {
 
     // Event for switching background and music
     if (event.target.classList.contains('data')) {
-        dataBtns.forEach(el => el.classList.remove('active_data'));
-        muteBtn.classList.remove('active')
-        event.target.classList.toggle('active_data')
-        video.classList.add('onVideo');
-        audio.pause();
-        pauseBtn.src = "https://img.icons8.com/fluent-systems-filled/24/000000/circled-pause.png";
 
-        if (event.target.classList.contains('beach')) {
-            video.src = "src/video/beach-1920x.mp4"
-            audio = new Audio(data[1].audio);
-            audio.play()
-        } else if (event.target.classList.contains('nature')) {
-            video.src = "src/video/Nature-1920x.mp4"
-            audio = new Audio(data[0].audio);
-            audio.play()
-        } else if (event.target.classList.contains('rain')) {
-            video.src = "src/video/rain.mp4"
-            audio = new Audio(data[2].audio);
-            audio.play()
-        }
+        // This animated transition
+        app.animate([{ opacity: 0.2 },
+                { opacity: 0, offset: 0.3 },
+                { opacity: 1 }
+            ],
+            2500);
+
+        // Timer in support of animated transition
+        setTimeout(() => {
+
+            dataBtns.forEach(el => el.classList.remove('active_data'));
+            muteBtn.classList.remove('active');
+            pauseBorder.classList.add('play');
+            event.target.classList.add('active_data');
+            video.classList.add('onVideo');
+            audio.pause();
+            pauseBtn.src = "https://img.icons8.com/fluent-systems-filled/24/000000/circled-pause.png";
+
+            if (event.target.classList.contains('beach')) {
+                video.src = "src/video/beach-1920x.mp4"
+                audio = new Audio(data[1].audio);
+                audio.play()
+            } else if (event.target.classList.contains('nature')) {
+                video.src = "src/video/Nature-1920x.mp4"
+                audio = new Audio(data[0].audio);
+                audio.play()
+            } else if (event.target.classList.contains('rain')) {
+                video.src = "src/video/rain.mp4"
+                audio = new Audio(data[2].audio);
+                audio.play()
+            }
+
+        }, 500)
+
     }
 
     // Event for the pause button and its work
     if (event.target.classList.contains('pause')) {
-
+        pauseBorder.classList.toggle('play')
         switch (event.target.src) {
             case "https://img.icons8.com/fluent-systems-filled/24/000000/circled-pause.png":
                 event.target.src = "https://img.icons8.com/fluent-systems-filled/24/000000/google-play.png"
